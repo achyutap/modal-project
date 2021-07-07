541e40e29ef1c3c6f63218de28ec13657c29331e
achyutap <achyutapataki@gmail.com>
2021-07-07 20:18:34 +0530

    simple-modal-by-vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="showModal" >click</button>
+  <button @click="showModalTwo" >click 2</button>
+  <div v-if="modal" style="width:100%; height:100%">
+    <Modal @close="showModal">
+      <template v-slot:links>
+        <a href="#">
+          fhej
+        </a>
+      </template>
+      <h1>
+        hey heyhey heyhey heyhey heyhey heyhey heyhey heyhey heyhey hey
+      </h1>
+    </Modal>
+
+  </div>
+  <div v-if="modal2" style="width:100%; height:100%">
+    <Modal @close="showModalTwo">
+      <h1>
+        gale
+      </h1>
+    </Modal>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal
+  },
+  data() {
+    return {
+      title: "hey",
+      showName: true,
+      modal: false,
+      modal2: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.modal = !this.modal;
+    },
+    showModalTwo() {
+      this.modal2 = !this.modal2;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="back" @click.self="closeModal()">
+    <div class="modal">
+      <slot></slot>
+      <div>
+          <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "desc"],
+
+  methods: {
+    showModal() {
+      this.modal = !this.modal;
+    },
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+body,
+html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+.back {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(167, 167, 167);
+}
+.modal {
+  width: 500px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgb(194, 74, 74);
+}
+</style>
